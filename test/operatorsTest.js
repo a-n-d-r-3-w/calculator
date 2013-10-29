99640a9df36519e7750bc8ae97ac135aa5760814
Andrew Liu <liu.anray@gmail.com>
2013-10-29 16:33:42 -0400

    Refactor test

--- test/operatorsTest.js
+++ test/operatorsTest.js
@@ -2,6 +2,10 @@ require(["../src/operators.js"], function (operators) {
     "use strict";
 
     test("Add two numbers", function () {
-        ok(operators.add(1, 1) === 2, "Passed!");
+        strictEqual(operators.add(1, 1), 2, "Addition works");
+    });
+
+    test("Subtract two numbers", function () {
+        strictEqual(operators.subtract(1, 1), 0, "Subtraction works");
     });
 });
