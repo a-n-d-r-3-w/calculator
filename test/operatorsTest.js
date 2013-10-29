6b3cd8e4e2e4cff6585183db496936e226970ebb
Andrew Liu <liu.anray@gmail.com>
2013-10-29 16:35:29 -0400

    Add test for multiply and divide

--- test/operatorsTest.js
+++ test/operatorsTest.js
@@ -2,10 +2,18 @@ require(["../src/operators.js"], function (operators) {
     "use strict";
 
     test("Add two numbers", function () {
-        strictEqual(operators.add(1, 1), 2, "Addition works");
+        strictEqual(operators.add(4, 6), 10, "Passed!");
     });
 
     test("Subtract two numbers", function () {
-        strictEqual(operators.subtract(1, 1), 0, "Subtraction works");
+        strictEqual(operators.subtract(1, 5), -4, "Passed!");
+    });
+
+    test("Multiply two numbers", function () {
+        strictEqual(operators.multiply(3, -4), -12, "Passed!");
+    });
+
+    test("Divide two numbers", function () {
+        strictEqual(operators.divide(7, 2), 3.5, "Passed!");
     });
 });
