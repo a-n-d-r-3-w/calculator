d5d94d5e2e1842d8a49c5c6d67576682f9527655
Andrew Liu <liu.anray@gmail.com>
2013-10-30 08:42:41 -0400

    Implement subtraction, multiplication, and division. Add test for subtraction. Use QUnit modules.

--- src/Operand.js
+++ src/Operand.js
@@ -2,9 +2,17 @@ define(["dojo", "dojo/_base/declare"], function (dojo, declare) {
     "use strict";
     return declare(null, {
         value: undefined,
-        add: function (secondOperand) {
-            // TODO: Assert that value is defined
+        plus: function (secondOperand) {
             return this.value + secondOperand;
+        },
+        minus: function (secondOperand) {
+            return this.value - secondOperand;
+        },
+        multiplyBy: function (secondOperand) {
+            return this.value * secondOperand;
+        },
+        divideBy: function (secondOperand) {
+            return this.value / secondOperand;
         },
         constructor: function (args) {
             dojo.mixin(this, args);
--- test/OperandTest.js
+++ test/OperandTest.js
@@ -1,6 +1,8 @@
 require(["../src/Operand.js"], function (Operand) {
     "use strict";
 
+    module("Addition");
+
     test("Create Operand", function () {
         var operand = new Operand();
         ok(operand, "Passed!");
@@ -8,17 +10,24 @@ require(["../src/Operand.js"], function (Operand) {
 
     test("Add two numbers", function () {
         var operand = new Operand({value: 1});
-        strictEqual(operand.add(3), 4, "Passed!");
+        strictEqual(operand.plus(3), 4, "Passed!");
     });
 
     test("Add: First operand is undefined", function () {
         var operand = new Operand({value: undefined});
-        deepEqual(operand.add(3), NaN, "Passed!");
+        deepEqual(operand.plus(3), NaN, "Passed!");
     });
 
     test("Add: Second operand is undefined", function () {
         var operand = new Operand({value: 2});
-        deepEqual(operand.add(undefined), NaN, "Passed!");
+        deepEqual(operand.plus(undefined), NaN, "Passed!");
+    });
+
+    module("Subtraction");
+
+    test("Subtract two numbers", function () {
+        var operand = new Operand({value : 1});
+        strictEqual(operand.minus(2), -1, "Passed!");
     });
 
 });
