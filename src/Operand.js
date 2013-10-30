3c5b3c231215c2b0aaf8e93677f3ed1fb70159d6
andrewaliu <liu.anray@gmail.com>
2013-10-29 23:09:49 -0400

    Add Operand class and test

---
+++ src/Operand.js
@@ -0,0 +1,7 @@
+define(["dojo/_base/declare"], function (declare) {
+    "use strict";
+    var Operand = declare("andrewaliu.Operand", null, {});
+    return {
+        Operand: Operand
+    };
+});
---
+++ test/OperandTest.js
@@ -0,0 +1,9 @@
+require(["../src/Operand.js"], function () {
+    "use strict";
+
+    test("Create Operand", function () {
+        var operand = new andrewaliu.Operand();
+        ok(operand, "Passed!");
+    });
+
+});
