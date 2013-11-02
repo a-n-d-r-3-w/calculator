24f8def585defccc5e001cbbaf6f243945ebe863
andrewaliu <liu.anray@gmail.com>
2013-11-01 23:48:02 -0400

    Add unit test

--- src/Accumulator.js
+++ src/Accumulator.js
@@ -1,6 +1,9 @@
 define(["dojo/_base/declare"], function (declare) {
     "use strict";
     return declare(null, {
-
+        value: undefined,
+        clear: function () {
+            this.value = "0";
+        }
     });
 });
