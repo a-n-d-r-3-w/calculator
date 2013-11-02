502118bc4b5974bc6179296b0f2cd5033ff82d2b
andrewaliu <liu.anray@gmail.com>
2013-11-02 00:31:51 -0400

    Add functionality to Accumulator

--- src/Accumulator.js
+++ src/Accumulator.js
@@ -5,8 +5,11 @@ define(["dojo/_base/declare"], function (declare) {
         clear: function () {
             this.value = "0";
         },
-        getContents: function () {
+        getContentsAsString: function () {
             return this.value;
+        },
+        getContentsAsFloat: function () {
+            return parseFloat(this.value);
         },
         addDigit: function (digit) {
             if (this.value === "0") {
--- test/AccumulatorTest.js
+++ test/AccumulatorTest.js
@@ -9,48 +9,48 @@ require(["../src/Accumulator.js"], function (Accumulator) {
         }
     });
 
-    test("clear and getContents", function () {
+    test("clear and getContentsAsFloat", function () {
         accumulator.clear();
-        strictEqual(accumulator.getContents(), "0");
+        strictEqual(accumulator.getContentsAsFloat(), 0);
     });
 
     test("add first non-zero digit", function () {
         accumulator.clear();
         accumulator.addDigit("4");
-        strictEqual(accumulator.getContents(), "4");
+        strictEqual(accumulator.getContentsAsFloat(), 4);
     });
 
     test("add two non-zero digits", function () {
         accumulator.clear();
         accumulator.addDigit("4");
         accumulator.addDigit("3");
-        strictEqual(accumulator.getContents(), "43");
+        strictEqual(accumulator.getContentsAsFloat(), 43);
     });
 
     test("add zero as first digit", function () {
         accumulator.clear();
         accumulator.addDigit("0");
-        strictEqual(accumulator.getContents(), "0");
+        strictEqual(accumulator.getContentsAsFloat(), 0);
     });
 
     test("add decimal point", function () {
         accumulator.clear();
         accumulator.addDecimalPoint();
-        strictEqual(accumulator.getContents(), "0.");
+        strictEqual(accumulator.getContentsAsString(), "0.");
     });
 
     test("try to add two decimal points", function () {
         accumulator.clear();
         accumulator.addDecimalPoint();
         accumulator.addDecimalPoint();
-        strictEqual(accumulator.getContents(), "0.");
+        strictEqual(accumulator.getContentsAsString(), "0.");
     });
 
     test("show negative sign", function () {
         accumulator.clear();
         accumulator.addDigit("2");
         accumulator.showNegativeSign();
-        strictEqual(accumulator.getContents(), "-2");
+        strictEqual(accumulator.getContentsAsFloat(), -2);
     });
 
     test("show negative sign twice", function () {
@@ -58,7 +58,7 @@ require(["../src/Accumulator.js"], function (Accumulator) {
         accumulator.addDigit("2");
         accumulator.showNegativeSign();
         accumulator.showNegativeSign();
-        strictEqual(accumulator.getContents(), "-2");
+        strictEqual(accumulator.getContentsAsFloat(), -2);
     });
 
     test("hide negative sign", function () {
@@ -66,13 +66,13 @@ require(["../src/Accumulator.js"], function (Accumulator) {
         accumulator.addDigit("2");
         accumulator.showNegativeSign();
         accumulator.hideNegativeSign();
-        strictEqual(accumulator.getContents(), "2");
+        strictEqual(accumulator.getContentsAsFloat(), 2);
     });
 
     test("show negative sign for zero", function () {
         accumulator.clear();
         accumulator.showNegativeSign();
-        strictEqual(accumulator.getContents(), "0");
+        strictEqual(accumulator.getContentsAsString(), "0");
     });
 
     test("user should not be able to enter more than 8 characters", function () {
@@ -86,7 +86,7 @@ require(["../src/Accumulator.js"], function (Accumulator) {
         accumulator.addDigit("1");
         accumulator.addDigit("1");
         accumulator.addDigit("1");
-        strictEqual(accumulator.getContents(), "11111111");
+        strictEqual(accumulator.getContentsAsFloat(), 11111111);
     });
 
 });
