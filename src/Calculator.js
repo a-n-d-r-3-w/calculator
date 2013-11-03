87fd243e0ffe3d19aebaddefd471a8f86569d3e3
andrewaliu <liu.anray@gmail.com>
2013-11-02 21:16:33 -0400

    Create Calculator.js

---
+++ src/Calculator.js
@@ -0,0 +1,20 @@
+require(
+    [
+        "../src/Accumulator.js",
+        "../src/Assertions.js",
+        "../src/EventHandlers.js",
+        "../src/Operand.js"
+    ],
+    function (
+        Accumulator,
+        Assertions,
+        EventHandlers,
+        Operand
+    ) {
+        "use strict";
+
+        // Create event handlers
+
+        //
+    }
+);
