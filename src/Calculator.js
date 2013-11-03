2c76b9ed5f6d2811cd05cb836f5f8eb6a33d66fd
andrewaliu <liu.anray@gmail.com>
2013-11-02 23:51:17 -0400

    Implement calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,51 +2,67 @@ require(["dojo/dom", "dojo/on", "dojo/domReady!"],
     function (dom, on) {
         "use strict";
 
-        var operand1 = 0,
-            operand2 = 0,
-            operator = "",
+        var operand1 = null,
+            operand2 = null,
+            operator = null,
+            accumulator = dom.byId("accumulator"),
+            needToClearAccumulator = false,
 
-            addDigitToAccumulatorText = function (digit) {
-                var accumulator = dom.byId("accumulator");
-                accumulator.innerHTML += digit;
+            enterDigit = function (digit) {
+                if (accumulator.innerHTML === "0" || needToClearAccumulator) {
+                    accumulator.innerHTML = digit;
+                    needToClearAccumulator = false;
+                } else {
+                    accumulator.innerHTML += digit;
+                }
+            },
+
+            Operator = {
+                ADD: 0,
+                SUBTRACT: 1,
+                MULTIPLY: 2,
+                DIVIDE: 3
             };
 
         on(dom.byId("clear"), "click", function () {
-            dom.byId("accumulator").innerHTML = "0";
+            operand1 = null;
+            operand2 = null;
+            operator = null;
+            accumulator.innerHTML = "0";
         });
         on(dom.byId("one"), "click", function () {
-            addDigitToAccumulatorText("1");
+            enterDigit("1");
         });
         on(dom.byId("two"), "click", function () {
-            addDigitToAccumulatorText("2");
+            enterDigit("2");
         });
         on(dom.byId("three"), "click", function () {
-            addDigitToAccumulatorText("3");
+            enterDigit("3");
         });
         on(dom.byId("four"), "click", function () {
-            addDigitToAccumulatorText("4");
+            enterDigit("4");
         });
         on(dom.byId("five"), "click", function () {
-            addDigitToAccumulatorText("5");
+            enterDigit("5");
         });
         on(dom.byId("six"), "click", function () {
-            addDigitToAccumulatorText("6");
+            enterDigit("6");
         });
         on(dom.byId("seven"), "click", function () {
-            addDigitToAccumulatorText("7");
+            enterDigit("7");
         });
         on(dom.byId("eight"), "click", function () {
-            addDigitToAccumulatorText("8");
+            enterDigit("8");
         });
         on(dom.byId("nine"), "click", function () {
-            addDigitToAccumulatorText("9");
+            enterDigit("9");
         });
         on(dom.byId("zero"), "click", function () {
-            addDigitToAccumulatorText("0");
+            enterDigit("0");
         });
         on(dom.byId("plusMinus"), "click", function () {
-            var accumulatorText = dom.byId("accumulator").innerHTML;
-            var startsWithMinus = accumulatorText.indexOf("-") === 0;
+            var accumulatorText = accumulator.innerHTML,
+                startsWithMinus = accumulatorText.indexOf("-") === 0;
             if (startsWithMinus) {
                 accumulator.innerHTML = accumulatorText.substring(1);
             } else {
@@ -54,38 +70,38 @@ require(["dojo/dom", "dojo/on", "dojo/domReady!"],
             }
         });
         on(dom.byId("decimalPoint"), "click", function () {
-            addDigitToAccumulatorText(".");
+            enterDigit(".");
         });
         on(dom.byId("plus"), "click", function () {
-            operator = "+";
-            operand1 = parseFloat(dom.byId("accumulator").innerHTML);
-            dom.byId("accumulator").innerHTML = "0";
+            operator = Operator.ADD;
+            operand1 = parseFloat(accumulator.innerHTML);
+            needToClearAccumulator = true;
         });
         on(dom.byId("minus"), "click", function () {
-            operator = "-";
-            operand1 = parseFloat(dom.byId("accumulator").innerHTML);
-            dom.byId("accumulator").innerHTML = "0";
+            operator = Operator.SUBTRACT;
+            operand1 = parseFloat(accumulator.innerHTML);
+            needToClearAccumulator = true;
         });
         on(dom.byId("multiply"), "click", function () {
-            operator = "*";
-            operand1 = parseFloat(dom.byId("accumulator").innerHTML);
-            dom.byId("accumulator").innerHTML = "0";
+            operator = Operator.MULTIPLY;
+            operand1 = parseFloat(accumulator.innerHTML);
+            needToClearAccumulator = true;
         });
         on(dom.byId("divide"), "click", function () {
-            operator = "/";
-            operand1 = parseFloat(dom.byId("accumulator").innerHTML);
-            dom.byId("accumulator").innerHTML = "0";
+            operator = Operator.DIVIDE;
+            operand1 = parseFloat(accumulator.innerHTML);
+            needToClearAccumulator = true;
         });
         on(dom.byId("equals"), "click", function () {
-            operand2 = parseFloat(dom.byId("accumulator").innerHTML);
-            if (operator === "+") {
-                dom.byId("accumulator").innerHTML = operand1 + operand2;
-            } else if (operator === "-") {
-                dom.byId("accumulator").innerHTML = operand1 - operand2;
-            } else if (operator === "*") {
-                dom.byId("accumulator").innerHTML = operand1 * operand2;
-            } else if (operator === "/") {
-                dom.byId("accumulator").innerHTML = operand1 / operand2;
+            operand2 = parseFloat(accumulator.innerHTML);
+            if (operator === Operator.ADD) {
+                accumulator.innerHTML = operand1 + operand2;
+            } else if (operator === Operator.SUBTRACT) {
+                accumulator.innerHTML = operand1 - operand2;
+            } else if (operator === Operator.MULTIPLY) {
+                accumulator.innerHTML = operand1 * operand2;
+            } else if (operator === Operator.DIVIDE) {
+                accumulator.innerHTML = operand1 / operand2;
             }
         });
     });
