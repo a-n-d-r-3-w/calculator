b482d40ab9661b69e1f26e557728c1e89c5625ce
andrewaliu <liu.anray@gmail.com>
2013-11-03 00:34:25 -0400

    Getting ready to move functions into Accumulator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,7 +5,39 @@ require(["dojo/dom", "dojo/on", "../src/Operator.js", "../src/Accumulator.js", "
         var operand1 = null,
             operand2 = null,
             operator = null,
-            accumulator = dom.byId("accumulator");
+            accumulator = dom.byId("accumulator"),
+            enterDigit = function (digit) {
+                if (accumulator.innerHTML === "0" || operator !== null) {
+                    accumulator.innerHTML = digit;
+                } else {
+                    accumulator.innerHTML += digit;
+                }
+            },
+
+            enterDecimalPoint = function () {
+                var decimalPointNotFound = accumulator.innerHTML.indexOf(".") === -1;
+                if (decimalPointNotFound) {
+                    accumulator.innerHTML += ".";
+                }
+            },
+
+            togglePlusMinus = function () {
+                var accumulatorText = accumulator.innerHTML,
+                    startsWithMinus = accumulatorText.indexOf("-") === 0;
+                if (startsWithMinus) {
+                    accumulator.innerHTML = accumulatorText.substring(1);
+                } else {
+                    accumulator.innerHTML = "-" + accumulator.innerHTML;
+                }
+            },
+
+            getValueAsFloat = function () {
+                return parseFloat(accumulator.innerHTML);
+            },
+
+            setInnerHtml = function (text) {
+                accumulator.innerHTML = text;
+            };
 
         on(dom.byId("clear"), "click", function () {
             operand1 = null;
