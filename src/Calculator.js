65bb980bbe2e857ef51b828f3177438aee47f2dc
Andrew Liu <liu.anray@gmail.com>
2013-11-05 08:33:41 -0500

    Enable 1 + 2 + 3

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,136 +4,96 @@ require(["dojo/dom", "dojo/on", "Operation.js", "Accumulator.js", "AccumulatorMo
         var operand1 = null,
             operand2 = null,
             operation = null,
-            thePreviousButtonPressedWasEquals = false,
-            clear = function () {
+            clearOperands = function () {
                 operand1 = null;
                 operand2 = null;
+            },
+            clearOperation = function () {
                 operation = null;
+            },
+            clearAccumulator = function () {
                 Accumulator.clear();
-                thePreviousButtonPressedWasEquals = false;
             };
 
         on(dom.byId("clear"), "click", function () {
-            clear();
+            clearOperands();
+            clearOperation();
+            clearAccumulator();
         });
         on(dom.byId("one"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("1");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("two"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("2");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("three"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("3");
-            thePreviousButtonPressedWasEquals = false;
-
         });
         on(dom.byId("four"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("4");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("five"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("5");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("six"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("6");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("seven"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("7");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("eight"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("8");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("nine"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("9");
-            thePreviousButtonPressedWasEquals = false;
         });
         on(dom.byId("zero"), "click", function () {
-            if (thePreviousButtonPressedWasEquals) {
-                clear();
-            }
             Accumulator.enterDigit("0");
-            thePreviousButtonPressedWasEquals = false;
         });
 
         on(dom.byId("plusMinus"), "click", function () {
             Accumulator.togglePlusMinus();
-            thePreviousButtonPressedWasEquals = false;
         });
 
         on(dom.byId("decimalPoint"), "click", function () {
             Accumulator.enterDecimalPoint();
-            thePreviousButtonPressedWasEquals = false;
         });
 
         on(dom.byId("plus"), "click", function () {
-            operation = Operation.add;
-            operand1 = Accumulator.getInnerHtmlAsFloat();
-            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_OPERAND_2);
-            thePreviousButtonPressedWasEquals = false;
+            if (operation === null) {
+                operation = Operation.add;
+                operand1 = Accumulator.getInnerHtmlAsFloat();
+            } else {
+                operand2 = Accumulator.getInnerHtmlAsFloat();
+                operand1 = operation(operand1, operand2);
+            }
+            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_AN_OPERAND);
         });
         on(dom.byId("minus"), "click", function () {
             operation = Operation.subtract;
             operand1 = Accumulator.getInnerHtmlAsFloat();
-            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_OPERAND_2);
-            thePreviousButtonPressedWasEquals = false;
+            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_AN_OPERAND);
         });
         on(dom.byId("multiply"), "click", function () {
             operation = Operation.multiply;
             operand1 = Accumulator.getInnerHtmlAsFloat();
-            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_OPERAND_2);
-            thePreviousButtonPressedWasEquals = false;
+            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_AN_OPERAND);
         });
         on(dom.byId("divide"), "click", function () {
             operation = Operation.divide;
             operand1 = Accumulator.getInnerHtmlAsFloat();
-            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_OPERAND_2);
-            thePreviousButtonPressedWasEquals = false;
+            Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_AN_OPERAND);
         });
 
         on(dom.byId("equals"), "click", function () {
             if (operation !== null) {
-                operand2 = thePreviousButtonPressedWasEquals ?
-                    operand2 // repeat the last operation
-                    : Accumulator.getInnerHtmlAsFloat();
+                operand2 = Accumulator.getInnerHtmlAsFloat();
                 var answer = operation(operand1, operand2);
-                operand1 = answer;
                 var answerAsString = answer.toString();
                 Accumulator.setInnerHtml(answerAsString);
-                Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_OPERAND_1);
-                thePreviousButtonPressedWasEquals = true;
+                Accumulator.setMode(AccumulatorMode.THE_NEXT_DIGIT_THE_USER_ENTERS_IS_THE_FIRST_DIGIT_OF_AN_OPERAND);
+                clearOperands();
+                clearOperation();
             }
         });
     });
