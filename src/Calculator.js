5c2a3603e93a2200b821b8fcbc8cde29c9adecbe
Andrew Liu <liu.anray@gmail.com>
2013-11-05 16:45:20 -0500

    refactor

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,7 +17,7 @@ require(["dojo/dom", "dojo/on", "Operator.js", "AccumulatorModel.js", "Accumulat
                     placeValueForNextDigit -= 1;
                 }
             },
-            addDigitAndUpdateAccumulatorView = function (digit) {
+            addDigitAndUpdateView = function (digit) {
                 addDigit(digit);
                 AccumulatorView.update();
             },
@@ -46,78 +46,89 @@ require(["dojo/dom", "dojo/on", "Operator.js", "AccumulatorModel.js", "Accumulat
                 operand2 = null;
                 operator = null;
                 placeValueForNextDigit = 0;
+            },
+            toggleSignAndUpdateView = function toggleSignAndUpdateView() {
+                AccumulatorModel.toggleSign();
+                AccumulatorView.update();
+            },
+            computeResultAndClearState = function () {
+                if (operator !== null) {
+                    operand2 = AccumulatorModel.getValue();
+                    var answer = operator(operand1, operand2);
+                    AccumulatorModel.setValue(answer);
+                    AccumulatorView.update();
+                    clearOperandsAndOperatorAndPlaceValue();
+                }
+            },
+            attachEventHandlers = function () {
+                on(dom.byId("clear"), "click", function () {
+                    clearOperandsAndOperatorAndPlaceValue();
+                    AccumulatorModel.clear();
+                    AccumulatorView.update();
+                });
+
+                on(dom.byId("number1"), "click", function () {
+                    addDigitAndUpdateView(1);
+                });
+                on(dom.byId("number2"), "click", function () {
+                    addDigitAndUpdateView(2);
+                });
+                on(dom.byId("number3"), "click", function () {
+                    addDigitAndUpdateView(3);
+                });
+                on(dom.byId("number4"), "click", function () {
+                    addDigitAndUpdateView(4);
+                });
+                on(dom.byId("number5"), "click", function () {
+                    addDigitAndUpdateView(5);
+                });
+                on(dom.byId("number6"), "click", function () {
+                    addDigitAndUpdateView(6);
+                });
+                on(dom.byId("number7"), "click", function () {
+                    addDigitAndUpdateView(7);
+                });
+                on(dom.byId("number8"), "click", function () {
+                    addDigitAndUpdateView(8);
+                });
+                on(dom.byId("number9"), "click", function () {
+                    addDigitAndUpdateView(9);
+                });
+                on(dom.byId("number0"), "click", function () {
+                    addDigitAndUpdateView(0);
+                });
+
+                on(dom.byId("plusMinus"), "click", function () {
+                    toggleSignAndUpdateView();
+                });
+
+                on(dom.byId("decimalPoint"), "click", function () {
+                    enteringFractionalPart = true;
+                    placeValueForNextDigit -= 1;
+                    AccumulatorView.addDecimalPoint();
+                });
+
+                on(dom.byId("plus"), "click", function () {
+                    setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.add);
+                });
+
+                on(dom.byId("minus"), "click", function () {
+                    setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.subtract);
+                });
+
+                on(dom.byId("multiply"), "click", function () {
+                    setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.multiply);
+                });
+
+                on(dom.byId("divide"), "click", function () {
+                    setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.divide);
+                });
+
+                on(dom.byId("equals"), "click", function () {
+                    computeResultAndClearState();
+                });
             };
 
-        on(dom.byId("clear"), "click", function () {
-            clearOperandsAndOperatorAndPlaceValue();
-            AccumulatorModel.clear();
-            AccumulatorView.update();
-        });
-        on(dom.byId("number1"), "click", function () {
-            addDigitAndUpdateAccumulatorView(1);
-        });
-        on(dom.byId("number2"), "click", function () {
-            addDigitAndUpdateAccumulatorView(2);
-        });
-        on(dom.byId("number3"), "click", function () {
-            addDigitAndUpdateAccumulatorView(3);
-        });
-        on(dom.byId("number4"), "click", function () {
-            addDigitAndUpdateAccumulatorView(4);
-        });
-        on(dom.byId("number5"), "click", function () {
-            addDigitAndUpdateAccumulatorView(5);
-        });
-        on(dom.byId("number6"), "click", function () {
-            addDigitAndUpdateAccumulatorView(6);
-        });
-        on(dom.byId("number7"), "click", function () {
-            addDigitAndUpdateAccumulatorView(7);
-        });
-        on(dom.byId("number8"), "click", function () {
-            addDigitAndUpdateAccumulatorView(8);
-        });
-        on(dom.byId("number9"), "click", function () {
-            addDigitAndUpdateAccumulatorView(9);
-        });
-        on(dom.byId("number0"), "click", function () {
-            addDigitAndUpdateAccumulatorView(0);
-        });
+        attachEventHandlers();
 
-        on(dom.byId("plusMinus"), "click", function () {
-            AccumulatorModel.toggleSign();
-            AccumulatorView.update();
-        });
-
-        on(dom.byId("decimalPoint"), "click", function () {
-            enteringFractionalPart = true;
-            placeValueForNextDigit -= 1;
-            AccumulatorView.addDecimalPoint();
-        });
-
-        on(dom.byId("plus"), "click", function () {
-            setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.add);
-        });
-
-        on(dom.byId("minus"), "click", function () {
-            setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.subtract);
-        });
-
-        on(dom.byId("multiply"), "click", function () {
-            setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.multiply);
-        });
-
-        on(dom.byId("divide"), "click", function () {
-            setOperatorAndSetOperandAndComputeIntermediateResultIfNecessary(Operator.divide);
-        });
-
-        on(dom.byId("equals"), "click", function () {
-            if (operator !== null) {
-                operand2 = AccumulatorModel.getValue();
-                var answer = operator(operand1, operand2);
-                AccumulatorModel.setValue(answer);
-                AccumulatorView.update();
-                clearOperandsAndOperatorAndPlaceValue();
-            }
-        });
     });
