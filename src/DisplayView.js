3219bfe8644200449ece71bdcc29223db8bd1b45
andrewaliu <liu.anray@gmail.com>
2013-11-05 22:12:19 -0500

    refactor

--- src/DisplayView.js
+++ src/DisplayView.js
@@ -13,6 +13,8 @@ define(["dojo/dom", "DisplayModel.js"], function (dom, DisplayModel) {
             if (decimalPointNotFound) {
                 view.innerHTML += ".";
             }
+            // TODO: If the decimal point is the first key of the next operand,
+            // then we should clear the view and show the decimal point only.
         }
 
     };
