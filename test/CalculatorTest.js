21072f64a66ba268a42ca2c6853aa11fc5584ad7
andrewaliu <liu.anray@gmail.com>
2013-11-07 21:46:05 -0500

    Set test context in setup

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -2,14 +2,14 @@ require(["dojo/dom", "dojo/_base/window", "dojo/domReady!"],
     function (dom, window) {
         "use strict";
 
-        module("Calculator");
+        module("Calculator", {
+            setup: function () {
+                var iframe = dom.byId("webPageUnderTest").contentWindow;
+                window.setContext(iframe.window, iframe.window.document);
+            }
+        });
 
         test("dummy", function () {
-            var iframe = dom.byId("webPageUnderTest").contentWindow;
-
-            // Change context from current window to iframe
-            window.setContext(iframe.window, iframe.window.document);
-
             var one = dom.byId("number1");
             var display = dom.byId("accumulator");
             console.log(display.innerHTML);
