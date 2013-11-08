970713f548314aff3ceb1e6b665a3e0ea2e1d219
Andrew Liu <liu.anray@gmail.com>
2013-11-08 12:32:35 -0500

    Add GUI tests

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -3,12 +3,16 @@ require(["dojo/dom", "dojo/_base/window", "dojo/domReady!"],
         "use strict";
 
         var display;
+        var fixtureIsSetUp = false;
 
         module("Calculator", {
             setup: function () {
-                var iframe = dom.byId("webPageUnderTest").contentWindow;
-                window.setContext(iframe.window, iframe.window.document);
-                display = dom.byId("accumulator");
+                if (!fixtureIsSetUp) {
+                    var iframe = dom.byId("webPageUnderTest").contentWindow;
+                    window.setContext(iframe.window, iframe.window.document);
+                    display = dom.byId("accumulator");
+                    fixtureIsSetUp = true;
+                }
             },
             teardown: function () {
                 dom.byId("clear").click();
@@ -36,6 +40,60 @@ require(["dojo/dom", "dojo/_base/window", "dojo/domReady!"],
             strictEqual(display.innerHTML, "9");
             dom.byId("equals").click();
             strictEqual(display.innerHTML, "-4");
+        });
+
+        test("2 * 7 = 14", function () {
+            dom.byId("number2").click();
+            strictEqual(display.innerHTML, "2");
+            dom.byId("multiply").click();
+            strictEqual(display.innerHTML, "2");
+            dom.byId("number7").click();
+            strictEqual(display.innerHTML, "7");
+            dom.byId("equals").click();
+            strictEqual(display.innerHTML, "14");
+        });
+
+        test("3 / 2 = 1.5", function () {
+            dom.byId("number3").click();
+            strictEqual(display.innerHTML, "3");
+            dom.byId("divide").click();
+            strictEqual(display.innerHTML, "3");
+            dom.byId("number2").click();
+            strictEqual(display.innerHTML, "2");
+            dom.byId("equals").click();
+            strictEqual(display.innerHTML, "1.5");
+        });
+
+        test("3 / 2 + 8 * 5 - 6 = 41.5", function () {
+            dom.byId("number3").click();
+            strictEqual(display.innerHTML, "3");
+
+            dom.byId("divide").click();
+            strictEqual(display.innerHTML, "3");
+
+            dom.byId("number2").click();
+            strictEqual(display.innerHTML, "2");
+
+            dom.byId("plus").click();
+            strictEqual(display.innerHTML, "2");
+
+            dom.byId("number8").click();
+            strictEqual(display.innerHTML, "8");
+
+            dom.byId("multiply").click();
+            strictEqual(display.innerHTML, "8");
+
+            dom.byId("number5").click();
+            strictEqual(display.innerHTML, "5");
+
+            dom.byId("minus").click();
+            strictEqual(display.innerHTML, "5");
+
+            dom.byId("number6").click();
+            strictEqual(display.innerHTML, "6");
+
+            dom.byId("equals").click();
+            strictEqual(display.innerHTML, "41.5");
         });
 
     });
