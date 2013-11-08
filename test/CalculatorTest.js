040a9d4515ba266eea97790848d3652cda6bd793
andrewaliu <liu.anray@gmail.com>
2013-11-07 21:44:44 -0500

    Add CalculatorTest that uses iframe and .click()

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -1,21 +1,21 @@
 require(["dojo/dom", "dojo/_base/window", "dojo/domReady!"],
     function (dom, window) {
-    "use strict";
+        "use strict";
 
-    module("Calculator");
+        module("Calculator");
 
-    test("dummy", function () {
-        var iframe = dom.byId("webPageUnderTest").contentWindow;
+        test("dummy", function () {
+            var iframe = dom.byId("webPageUnderTest").contentWindow;
 
-        // Change context from current window to iframe
-        window.setContext(iframe.window, iframe.window.document);
+            // Change context from current window to iframe
+            window.setContext(iframe.window, iframe.window.document);
 
-        var one = dom.byId("number1");
-        var display = dom.byId("accumulator");
-        console.log(display.innerHTML);
-        one.click();
-        console.log(display.innerHTML);
-        ok(true); // dummy assertion
+            var one = dom.byId("number1");
+            var display = dom.byId("accumulator");
+            console.log(display.innerHTML);
+            one.click();
+            console.log(display.innerHTML);
+            ok(true); // dummy assertion
+        });
+
     });
-
-});
