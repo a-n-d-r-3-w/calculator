e6e6634544740a16677285248ebe21d73d2f7d25
andrewaliu <liu.anray@gmail.com>
2013-11-07 22:10:53 -0500

    Add CalculatorTester

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -2,20 +2,29 @@ require(["dojo/dom", "dojo/_base/window", "dojo/domReady!"],
     function (dom, window) {
         "use strict";
 
+        var display;
+
         module("Calculator", {
             setup: function () {
                 var iframe = dom.byId("webPageUnderTest").contentWindow;
                 window.setContext(iframe.window, iframe.window.document);
+                display = dom.byId("accumulator");
+            },
+            teardown: function () {
+                dom.byId("clear").click();
+                strictEqual(display.innerHTML, "0");
             }
         });
 
-        test("dummy", function () {
-            var one = dom.byId("number1");
-            var display = dom.byId("accumulator");
-            console.log(display.innerHTML);
-            one.click();
-            console.log(display.innerHTML);
-            ok(true); // dummy assertion
+        test("1 + 4 = 5", function () {
+            dom.byId("number1").click();
+            strictEqual(display.innerHTML, "1");
+            dom.byId("plus").click();
+            strictEqual(display.innerHTML, "1");
+            dom.byId("number4").click();
+            strictEqual(display.innerHTML, "4");
+            dom.byId("equals").click();
+            strictEqual(display.innerHTML, "5");
         });
 
     });
