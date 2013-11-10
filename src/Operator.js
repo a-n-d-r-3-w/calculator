e9dcd7affd30661ca2273c2ba606aec2872decf3
andrewaliu <liu.anray@gmail.com>
2013-11-09 19:32:46 -0500

    Partial fix for Issue #1

--- src/Operator.js
+++ src/Operator.js
@@ -1,21 +1,19 @@
-define(["../lib/sinful.js"], function () {
+define({
 
-    return {
-        add: function (operand1, operand2) {
-            return Math.add(operand1, operand2);
-        },
+    add: function (operand1, operand2) {
+        return operand1 + operand2;
+    },
 
-        subtract: function (operand1, operand2) {
-            return Math.sub(operand1, operand2);
-        },
+    subtract: function (operand1, operand2) {
+        return operand1 - operand2;
+    },
 
-        multiply: function (operand1, operand2) {
-            return Math.mul(operand1, operand2);
-        },
+    multiply: function (operand1, operand2) {
+        return operand1 * operand2;
+    },
 
-        divide: function (operand1, operand2) {
-            return Math.div(operand1, operand2);
-        }
+    divide: function (operand1, operand2) {
+        return operand1 / operand2;
     }
 
 });
