111240310a1d4f02c7cdd2fc27c5f1d709262bef
andrewaliu <liu.anray@gmail.com>
2013-11-10 01:12:46 -0500

    Completed re-implementation using OOD

--- src/duckduckgo/DuckDuckGoApiExample.js
+++
@@ -1,9 +0,0 @@
-require(["../../lib/dojo-release-1.9.1/dojo/request"], function(request){
-    request("http://api.duckduckgo.com/?q=1%2B2&format=json&pretty=1&t=calculator&no_html=1&callback=?").then(function(data){
-        alert(data);
-    }, function(err){
-        // handle an error condition
-    }, function(evt){
-        // handle a progress event
-    });
-});
