92cffe3dfca6873ddaa06394c45faa24189b9ce3
andrewaliu <liu.anray@gmail.com>
2013-11-10 01:09:53 -0500

    Gradually re-implementing features

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,10 +17,20 @@ require([
             operator = null,
             activeOperand = null,
             result = null,
+            lastButtonPressedWasEquals = false,
 
             display = dom.byId("display"),
 
             addDigit = function (digit) {
+                if (lastButtonPressedWasEquals) {
+                    operand1.setText("0");
+                    operand2.setText("0");
+                    operator = null;
+                    activeOperand = operand1;
+                    display.innerHTML = activeOperand.getText();
+                    result = null;
+                    lastButtonPressedWasEquals = false;
+                }
                 activeOperand.appendNumber(digit);
                 display.innerHTML = activeOperand.getText();
             },
@@ -48,6 +58,7 @@ require([
                     activeOperand = operand1;
                     display.innerHTML = activeOperand.getText();;
                     result = null;
+                    lastButtonPressedWasEquals = false;
                 });
             },
 
@@ -66,11 +77,13 @@ require([
                 on(dom.byId("toggleSign"), "click", function () {
                     activeOperand.toggleSign();
                     display.innerHTML = activeOperand.getText();
+                    lastButtonPressedWasEquals = false;
                 });
 
                 on(dom.byId("decimalPoint"), "click", function () {
                     activeOperand.appendDecimalPoint();
                     display.innerHTML = activeOperand.getText();
+                    lastButtonPressedWasEquals = false;
                 });
             },
 
@@ -79,46 +92,51 @@ require([
                     if (operator !== null) {
                         computeResult();
                         operand1.setText(result.getText());
-                        operand2.setText("");
+                        operand2.setText("0");
                     }
                     operator = Operator.PLUS;
                     activeOperand = operand2;
+                    lastButtonPressedWasEquals = false;
                 });
 
                 on(dom.byId("minus"), "click", function () {
                     if (operator !== null) {
                         computeResult();
                         operand1.setText(result.getText());
-                        operand2.setText("");
+                        operand2.setText("0");
                     }
                     operator = Operator.MINUS;
                     activeOperand = operand2;
+                    lastButtonPressedWasEquals = false;
                 });
 
                 on(dom.byId("multiplyBy"), "click", function () {
                     if (operator !== null) {
                         computeResult();
                         operand1.setText(result.getText());
-                        operand2.setText("");
+                        operand2.setText("0");
                     }
                     operator = Operator.MULTIPLY_BY;
                     activeOperand = operand2;
+                    lastButtonPressedWasEquals = false;
                 });
 
                 on(dom.byId("divideBy"), "click", function () {
                     if (operator !== null) {
                         computeResult();
                         operand1.setText(result.getText());
-                        operand2.setText("");
+                        operand2.setText("0");
                     }
                     operator = Operator.DIVIDE_BY;
                     activeOperand = operand2;
+                    lastButtonPressedWasEquals = false;
                 });
             },
 
             attachEventHandlerForEqualsButton = function () {
                 on(dom.byId("equals"), "click", function () {
                     computeResult();
+                    lastButtonPressedWasEquals = true;
                 });
 
             },
