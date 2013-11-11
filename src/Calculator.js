3129a2803554614613be672ac5fa85c6dd9ceeda
Andrew Liu <liu.anray@gmail.com>
2013-11-11 13:54:31 -0500

    refactor

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,71 +3,28 @@ require([
     "dojo/on",
     "CalculatorNumber.js",
     "Operator.js",
-    "Display.js",
+    "CalculatorObject.js",
     "dojo/domReady!"],
 
     function (dom,
               on,
               CalculatorNumber,
               Operator,
-              Display) {
+              CalculatorObject) {
 
         "use strict";
 
-        var operand1,
-            operand2,
-            operator,
-            activeOperand,
-            result,
-            lastButtonPressedWasEquals,
-            display,
-
-            clear = function () {
-                operand1 = new CalculatorNumber("0");
-                operand2 = new CalculatorNumber("");
-                operator = null;
-                activeOperand = operand1;
-                display = new Display(activeOperand.getText());
-                result = new CalculatorNumber("");
-                lastButtonPressedWasEquals = false;
-            },
-
-            addDigit = function (digit) {
-                if (lastButtonPressedWasEquals) {
-                    clear();
-                }
-                activeOperand.appendNumber(digit);
-                display.setText(activeOperand.getText());
-            },
-
-            operatorExists = function () {
-                return operator !== null;
-            },
-
-            computeResult = function () {
-                if (operator === Operator.PLUS) {
-                    result = operand1.plus(operand2);
-                } else if (operator === Operator.MINUS) {
-                    result = operand1.minus(operand2);
-                } else if (operator === Operator.MULTIPLY_BY) {
-                    result = operand1.multiplyBy(operand2);
-                } else if (operator === Operator.DIVIDE_BY) {
-                    result = operand1.divideBy(operand2);
-                } else {
-                    throw new Error("Unexpected operator.");
-                }
-                display.setText(result.getText());
-            },
+        var calculator = new CalculatorObject(),
 
             attachEventHandlerForClearButton = function () {
                 on(dom.byId("clear"), "click", function () {
-                    clear();
+                    calculator.clear();
                 });
             },
 
             attachEventHandlerForNumberButton = function (digit) {
                 on(dom.byId("number" + digit), "click", function () {
-                    addDigit(digit);
+                    calculator.addDigit(digit);
                 });
             },
 
@@ -78,74 +35,35 @@ require([
                 }
 
                 on(dom.byId("toggleSign"), "click", function () {
-                    activeOperand.toggleSign();
-                    display.setText(activeOperand.getText());
-                    lastButtonPressedWasEquals = false;
+                    calculator.toggleSign();
                 });
 
                 on(dom.byId("decimalPoint"), "click", function () {
-                    if (lastButtonPressedWasEquals) {
-                        clear();
-                    }
-                    activeOperand.appendDecimalPoint();
-                    display.setText(activeOperand.getText());
+                    calculator.addDecimalPoint();
                 });
             },
 
             attachEventHandlersForOperatorButtons = function () {
                 on(dom.byId("plus"), "click", function () {
-                    if (operatorExists()) {
-                        computeResult();
-                        operand1.setText(result.getText());
-                        operand2.setText("0");
-                    }
-                    operator = Operator.PLUS;
-                    activeOperand = operand2;
-                    lastButtonPressedWasEquals = false;
+                    calculator.plus();
                 });
 
                 on(dom.byId("minus"), "click", function () {
-                    if (operatorExists()) {
-                        computeResult();
-                        operand1.setText(result.getText());
-                        operand2.setText("0");
-                    }
-                    operator = Operator.MINUS;
-                    activeOperand = operand2;
-                    lastButtonPressedWasEquals = false;
+                    calculator.minus();
                 });
 
                 on(dom.byId("multiplyBy"), "click", function () {
-                    if (operatorExists()) {
-                        computeResult();
-                        operand1.setText(result.getText());
-                        operand2.setText("0");
-                    }
-                    operator = Operator.MULTIPLY_BY;
-                    activeOperand = operand2;
-                    lastButtonPressedWasEquals = false;
+                    calculator.multiplyBy();
                 });
 
                 on(dom.byId("divideBy"), "click", function () {
-                    if (operatorExists()) {
-                        computeResult();
-                        operand1.setText(result.getText());
-                        operand2.setText("0");
-                    }
-                    operator = Operator.DIVIDE_BY;
-                    activeOperand = operand2;
-                    lastButtonPressedWasEquals = false;
+                    calculator.divideBy();
                 });
             },
 
             attachEventHandlerForEqualsButton = function () {
                 on(dom.byId("equals"), "click", function () {
-                    if (operand1.getText() !== "" &&
-                        operatorExists() &&
-                        operand2.getText() !== "") {
-                        computeResult();
-                    }
-                    lastButtonPressedWasEquals = true;
+                    calculator.equals();
                 });
 
             },
@@ -158,5 +76,5 @@ require([
             };
 
         attachEventHandlers();
-        clear();
+        calculator.clear();
     });
---
+++ src/CalculatorObject.js
@@ -0,0 +1,118 @@
+define(["dojo/dom", "dojo/_base/declare", "CalculatorNumber.js", "Operator.js", "Display.js"],
+    function (dom, declare, CalculatorNumber, Operator, Display) {
+        "use strict";
+
+        var operand1,
+            operand2,
+            operator,
+            activeOperand,
+            result,
+            lastButtonPressedWasEquals,
+            display;
+
+        return declare(null, {
+            clear: function () {
+                operand1 = new CalculatorNumber("0");
+                operand2 = new CalculatorNumber("");
+                operator = null;
+                activeOperand = operand1;
+                display = new Display(activeOperand.getText());
+                result = new CalculatorNumber("");
+                lastButtonPressedWasEquals = false;
+            },
+
+            addDigit: function (digit) {
+                if (lastButtonPressedWasEquals) {
+                    this.clear();
+                }
+                activeOperand.appendNumber(digit);
+                display.setText(activeOperand.getText());
+            },
+
+            operatorExists: function () {
+                return operator !== null;
+            },
+
+            computeResult: function () {
+                if (operator === Operator.PLUS) {
+                    result = operand1.plus(operand2);
+                } else if (operator === Operator.MINUS) {
+                    result = operand1.minus(operand2);
+                } else if (operator === Operator.MULTIPLY_BY) {
+                    result = operand1.multiplyBy(operand2);
+                } else if (operator === Operator.DIVIDE_BY) {
+                    result = operand1.divideBy(operand2);
+                } else {
+                    throw new Error("Unexpected operator.");
+                }
+                display.setText(result.getText());
+            },
+
+            toggleSign: function () {
+                activeOperand.toggleSign();
+                display.setText(activeOperand.getText());
+                lastButtonPressedWasEquals = false;
+            },
+
+            addDecimalPoint: function () {
+                if (lastButtonPressedWasEquals) {
+                    this.clear();
+                }
+                activeOperand.appendDecimalPoint();
+                display.setText(activeOperand.getText());
+            },
+
+            plus: function () {
+                if (this.operatorExists()) {
+                    this.computeResult();
+                    operand1.setText(result.getText());
+                    operand2.setText("0");
+                }
+                operator = Operator.PLUS;
+                activeOperand = operand2;
+                lastButtonPressedWasEquals = false;
+            },
+
+            minus: function () {
+                if (this.operatorExists()) {
+                    this.computeResult();
+                    operand1.setText(result.getText());
+                    operand2.setText("0");
+                }
+                operator = Operator.MINUS;
+                activeOperand = operand2;
+                lastButtonPressedWasEquals = false;
+            },
+
+            multiplyBy: function () {
+                if (this.operatorExists()) {
+                    this.computeResult();
+                    operand1.setText(result.getText());
+                    operand2.setText("0");
+                }
+                operator = Operator.MULTIPLY_BY;
+                activeOperand = operand2;
+                lastButtonPressedWasEquals = false;
+            },
+
+            divideBy: function () {
+                if (this.operatorExists()) {
+                    this.computeResult();
+                    operand1.setText(result.getText());
+                    operand2.setText("0");
+                }
+                operator = Operator.DIVIDE_BY;
+                activeOperand = operand2;
+                lastButtonPressedWasEquals = false;
+            },
+
+            equals: function () {
+                if (operand1.getText() !== "" &&
+                    this.operatorExists() &&
+                    operand2.getText() !== "") {
+                    this.computeResult();
+                }
+                lastButtonPressedWasEquals = true;
+            }
+        });
+    });
